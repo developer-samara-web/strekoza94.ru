401543acaef28e18a5283e41943645f405bd148e
developer-samara-web <developer.samara.web@gmail.com>
2026-07-29 19:04:08 +0400

    update: Add Section components on Hero screen

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -2,17 +2,18 @@ import './Hero.css';
 
 import Image from "next/image";
 import Button from '@/ui/Button/Button';
+import Section from '@/ui/Section/Section';
 
 const Hero = () => {
 	return (
-		<section className="hero">
+		<Section className="hero">
 			<div className="hero__inner">
 				<div className="hero__title">Учитесь и развивайтесь -<br></br> каждый день!</div>
 				<div className="hero__description">Сделайте обучение детей увлекательным и интерактивным с помощью интересных уроков,<br></br>игр и творческих занятий.</div>
 				<Button type="link" className="button button-md" url="#" text="Забронировать праздник" />
 			</div>
 			<Image className="hero__image" src="/images/hero/hero.png" width="1400" height="500" alt="hero" />
-		</section>
+		</Section>
 	);
 }
 
