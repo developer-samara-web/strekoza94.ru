5a7462b40121b5a8d177df9782af8fcf8cfbaa6a
developer-samara-web <developer.samara.web@gmail.com>
2026-07-29 19:03:11 +0400

    update: The first screen of the site has been added

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -1,5 +1,19 @@
+import './Hero.css';
+
+import Image from "next/image";
+import Button from '@/ui/Button/Button';
+
 const Hero = () => {
-	return <section>Hero</section>;
+	return (
+		<section className="hero">
+			<div className="hero__inner">
+				<div className="hero__title">Учитесь и развивайтесь -<br></br> каждый день!</div>
+				<div className="hero__description">Сделайте обучение детей увлекательным и интерактивным с помощью интересных уроков,<br></br>игр и творческих занятий.</div>
+				<Button type="link" className="button button-md" url="#" text="Забронировать праздник" />
+			</div>
+			<Image className="hero__image" src="/images/hero/hero.png" width="1400" height="500" alt="hero" />
+		</section>
+	);
 }
 
 export default Hero;
