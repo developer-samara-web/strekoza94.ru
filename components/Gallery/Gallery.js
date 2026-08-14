9ab6ee49f652e640a0199c0a016ac9b1619f6c76
developer-samara-web <developer.samara.web@gmail.com>
2026-08-15 00:51:58 +0400

    refactor: improve gallery component structure and readability

--- components/Gallery/Gallery.js
+++ components/Gallery/Gallery.js
@@ -1,26 +1,38 @@
 import "./Gallery.css";
 
-import Title from "@/ui/Title/Title";
 import Section from "@/ui/Section/Section";
 import GalleryList from "@/components/Gallery/GalleryList";
 
 const Gallery = () => {
 
-	const list = [
-		"/images/gallery/1.jpg",
-		"/images/gallery/2.jpg",
-		"/images/gallery/3.jpg",
-		"/images/gallery/4.jpg",
-		"/images/gallery/5.jpg",
-		"/images/gallery/6.jpg",
-	];
+	const { header, items } = {
+		header: {
+			title: "Фотогалерея",
+			description: "Превращаем каждый праздник в приключение",
+			text: "В нашем центре каждый праздник становится ярким событием. Мы организуем мероприятия для детей любого возраста: от уютных утренников и дней рождения до масштабных шоу-программ, выпускных и новогодних праздников",
+			background: "bg-[#008f3f]",
+			color: "text-[#008f3f]"
+		},
+		items: [
+			"/images/gallery/1.jpg",
+			"/images/gallery/2.jpg",
+			"/images/gallery/3.jpg",
+			"/images/gallery/4.jpg",
+			"/images/gallery/5.jpg",
+			"/images/gallery/6.jpg",
+			"/images/gallery/1.jpg",
+			"/images/gallery/2.jpg",
+			"/images/gallery/3.jpg",
+			"/images/gallery/4.jpg",
+			"/images/gallery/5.jpg",
+			"/images/gallery/6.jpg",
+		]
+	}
 
 	return (
-		<Section className="gallery">
+		<Section id="gallery" className="gallery" header={header}>
 			<div className="gallery__inner">
-				<Title title="Фотогалерея" description="Превращаем каждый праздник в приключение." color="text-[#008f3f]" />
-				<div className="gallery__heading">В нашем центре каждый праздник становится ярким событием. Мы организуем мероприятия для детей любого возраста: от уютных утренников и дней рождения до масштабных шоу-программ, выпускных и новогодних праздников.</div>
-				<GalleryList list={list} />
+				<GalleryList list={items} />
 			</div>
 		</Section>
 	);
