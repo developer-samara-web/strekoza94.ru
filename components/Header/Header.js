11e3c31ed8cfa0b100ee0bff6e37c9be7d6f57ca
developer-samara-web <developer.samara.web@gmail.com>
2026-08-15 00:49:11 +0400

    refactor: reorganize CSS and update header structure

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,48 +1,53 @@
 import "./Header.css";
 
 import Image from "next/image";
-import Navbar from "@/components/ui/Navbar/Navbar";
-import Button from "@/ui/Button/Button";
-import NavbarMobile from "@/ui/Navbar/NavbarMobile";
 import Section from "@/ui/Section/Section";
+import Button from "@/ui/Button/Button";
+import Navbar from "@/components/ui/Navbar/Navbar";
+import NavbarMobile from "@/ui/Navbar/NavbarMobile";
+import { PhoneIcon } from '@heroicons/react/24/solid';
 
 const Header = () => {
 
-	const navbar = [
-		{
-			title: "Мероприятия",
-			href: "#"
+	const { menu, contacts, button } = {
+		menu: [
+			{
+				title: "Мероприятия",
+				href: "#events"
+			},
+			{
+				title: "Услуги и цены",
+				href: "#price"
+			},
+			{
+				title: "Галерея",
+				href: "#"
+			},
+			{
+				title: "Вопросы и ответы",
+				href: "#"
+			},
+			{
+				title: "Контакты",
+				href: "#"
+			},
+		],
+		contacts: {
+			address: "Грабцевское шоссе, 68",
+			phone: "+7 (920) 616-02-65",
 		},
-		{
-			title: "Услуги и цены",
-			href: "#"
-		},
-		{
-			title: "Галерея",
-			href: "#"
-		},
-		{
-			title: "Вопросы и ответы",
-			href: "#"
-		},
-		{
-			title: "Контакты",
-			href: "#"
-		},
-	]
-
-	const contacts = {
-		address: "Грабцевское шоссе, 68",
-		phone: "+7 (920) 616-02-65",
-		buttonName: "Позвонить",
-		buttonPhone: "tel:+79991234567"
+		button: {
+			name: "Позвонить",
+			phone: "tel:+79991234567",
+			icon: <PhoneIcon className="size-3" />
+		}
 	}
 
 	return (
 		<Section className="header">
 			<div className="header__inner">
 				<div className="header__row">
-					<Image src="/images/logo.png" width={160} height={100} loading="eager" alt="logo" />
+					<Image className="header__logo" src="/images/logo.png" width={208} height={40} loading="eager" alt="logo" />
 					<div className="header__content">
 						<span className="header__address">{contacts.address}</span>
 						<span className="header__phone">{contacts.phone}</span>
@@ -51,10 +56,17 @@ const Header = () => {
 			</div>
 			<div className="header__inner">
 				<div className="header__row header__row--bottom">
-					<NavbarMobile navbar={navbar} />
-					<Navbar navbar={navbar} />
+					<NavbarMobile navbar={menu} />
+					<Navbar navbar={menu} />
 					<div className="header__content">
-						<Button type="link" className="button button-sm button-primary" url={contacts.buttonPhone} text="Позвонить" />
+						<Button
+							type="link"
+							className="button button-sm button-primary"
+							url={button.phone}
+							icon={button.icon}
+							iconPosition="left"
+							text={button.name}
+						/>
 					</div>
 				</div>
 			</div>
