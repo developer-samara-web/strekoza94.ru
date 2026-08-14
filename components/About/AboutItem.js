fcb4db9f6c18cd35d247f07bcc9aec9faa0ca853
developer-samara-web <developer.samara.web@gmail.com>
2026-08-15 00:50:36 +0400

    refactor: reorganize styles and about section structure

--- components/About/AboutItem.js
+++ components/About/AboutItem.js
@@ -1,14 +1,14 @@
 import Image from "next/image";
 
-const AboutItem = ({ title, description, color, image }) => {
+const AboutItem = ({ title, description, color, background, border, image }) => {
 	return (
-		<div className="about__item" style={{ backgroundColor: color }}>
+		<div className="about__item" style={{ backgroundColor: background, borderColor: border, color: color }}>
 			<div className="about-item__image">
-				<Image className="about-item__picture" src={image} width={160} height={160} alt="about" />
+				<Image className="about-item__picture" src={image} width={160} height={160} alt={title} loading="eager" />
 			</div>
 			<div className="about-item__content">
-				<h3 className="about-item__title">{title}</h3>
-				<div>{description}</div>
+				<h3 className={`about-item__title ${color}`}>{title}</h3>
+				<div className="about-item__description">{description}</div>
 			</div>
 		</div>
 	);
--- components/About/AboutList.js
+++ components/About/AboutList.js
@@ -2,9 +2,9 @@ import AboutItem from "@/components/About/AboutItem";
 
 const AboutList = ({ abouts }) => {
 	return (
-		<div className="about-list">
-			{abouts && abouts.map(({ title, description, color, image }, id) => (
-				<AboutItem key={id} title={title} description={description} color={color} image={image} />
+		<div className="about__list">
+			{abouts && abouts.map(({ title, description, color, background, border, image }, id) => (
+				<AboutItem key={id} title={title} description={description} color={color} background={background} border={border} image={image} />
 			))}
 		</div>
 	);
