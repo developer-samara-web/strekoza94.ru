e9f8b621bc589ce2182502b5b47bef4c61cc3c35
developer-samara-web <developer.samara.web@gmail.com>
2026-08-15 00:49:50 +0400

    refactor: reorganize CSS for better readability and maintainability

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -3,16 +3,30 @@ import './Hero.css';
 import Image from "next/image";
 import Button from '@/ui/Button/Button';
 import Section from '@/ui/Section/Section';
+import { ArrowLongRightIcon } from '@heroicons/react/24/solid';
 
 const Hero = () => {
+	const { header, button } = {
+		header: {
+			title: "Учитесь и развивайтесь - каждый день!",
+			description: "Сделайте обучение детей увлекательным и интерактивным с помощью интересных уроков, игр и творческих занятий."
+		},
+		button: {
+			type: "link",
+			name: "Узнать больше",
+			icon: <ArrowLongRightIcon className="size-5" />,
+			url: "#about"
+		}
+	}
+
 	return (
-		<Section className="hero">
+		<Section header={false} className="hero">
 			<div className="hero__inner">
-				<div className="hero__title">Учитесь и развивайтесь -<br></br> каждый день!</div>
-				<div className="hero__description">Сделайте обучение детей увлекательным и интерактивным с помощью интересных уроков,<br></br>игр и творческих занятий.</div>
-				<Button type="link" className="button button-md button-primary" url="#" text="Забронировать праздник" />
+				<div className="hero__title">{header.title}</div>
+				<div className="hero__description">{header.description}</div>
+				<Button type={button.type} className="button button-md button-primary" url={button.url} text={button.name} icon={button.icon} />
 			</div>
-			<Image className="hero__image" src="/images/hero/hero.png" width="1400" height="500" alt="hero" />
+			<Image className="hero__image" src="/images/hero/hero.png" width="1926" height="816" loading="eager" alt="Учитесь и развивайтесь - каждый день!" />
 		</Section>
 	);
 }
